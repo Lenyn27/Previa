02ac2d14d34840e69c970b4f2ef2c8e1c3c4ad7b
Lenyn27 <1369330@senati.pe>
2022-11-26 19:02:31 -0500

    Actualisacion de BD y Imagenes de los datos

--- veterinaria.sql
+++ veterinaria.sql
@@ -23,16 +23,15 @@ DROP TABLE IF EXISTS `card`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `card` (
-  `idcard` int NOT NULL AUTO_INCREMENT,
+  `idcard` int NOT NULL,
   `id_Mascota` int NOT NULL,
   `nombre_mascota` varchar(25) DEFAULT NULL,
   `edad` int DEFAULT NULL,
-  `sexo` tinyint(1) DEFAULT NULL,
+  `sexo` varchar(15) DEFAULT NULL,
   `especie` varchar(25) DEFAULT NULL,
   `raza` varchar(25) DEFAULT NULL,
   `peso` float DEFAULT NULL,
   `color_masc` varchar(15) DEFAULT NULL,
-  `fecha_nacimiento` date DEFAULT NULL,
   `fecha_vacunacion` date DEFAULT NULL,
   `tipo_vacuna` varchar(25) DEFAULT NULL,
   `observaciones` varchar(40) DEFAULT NULL,
@@ -48,6 +47,7 @@ CREATE TABLE `card` (
 
 LOCK TABLES `card` WRITE;
 /*!40000 ALTER TABLE `card` DISABLE KEYS */;
+INSERT INTO `card` VALUES (1,1,'Zeus',4,'Macho','Perro','Grandanes',54,'negro','2002-03-22','general','ninguna'),(2,1,'Pelusa',10,'Hembra','Perro','chihuahua',4,'rubio alvino','2002-04-22','general','tiene tos'),(3,2,'Patitas',9,'Hembra','Gato','Abisinio',6,'marron','2002-05-22','general','ninguna'),(4,3,'cheyco',8,'Macho','Perro','Chusco',12,'negraso','2002-06-22','general','ninguna'),(5,4,'Michifus',1,'Macho','gato','Balines',3,'Anaranjado','2002-07-22','general','ninguna'),(6,5,'tomas',10,'Macho','gato','chuscofino',7,'gris','2002-08-22','general','ninguna'),(7,5,'Felipe',6,'Macho','ave','loro',1,'verde','2002-09-22','general','caidaplumas ');
 /*!40000 ALTER TABLE `card` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -59,16 +59,15 @@ DROP TABLE IF EXISTS `mascotas`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `mascotas` (
-  `idmascota` int NOT NULL AUTO_INCREMENT,
+  `idmascota` int NOT NULL,
   `id_Propietario` int NOT NULL,
   `nombre_mascota` varchar(25) DEFAULT NULL,
   `edad` int DEFAULT NULL,
-  `sexo` tinyint(1) DEFAULT NULL,
+  `sexo` varchar(15) DEFAULT NULL,
   `especie` varchar(25) DEFAULT NULL,
   `raza` varchar(25) DEFAULT NULL,
   `peso` float DEFAULT NULL,
   `color_masc` varchar(15) DEFAULT NULL,
-  `fecha_nacimiento` date DEFAULT NULL,
   `vacunaciones` date DEFAULT NULL,
   PRIMARY KEY (`idmascota`),
   KEY `id_Propietario` (`id_Propietario`),
@@ -82,6 +81,7 @@ CREATE TABLE `mascotas` (
 
 LOCK TABLES `mascotas` WRITE;
 /*!40000 ALTER TABLE `mascotas` DISABLE KEYS */;
+INSERT INTO `mascotas` VALUES (1,1,'Zeus',4,'Macho','Perro','Grandanes',54,'negro','2002-03-22'),(2,1,'Pelusa',10,'Hembra','Perro','chihuahua',4,'rubio alvino','2002-04-22'),(3,2,'Patitas',9,'Hembra','Gato','Abisinio',6,'marron','2002-05-22'),(4,3,'cheyco',8,'Macho','Perro','Chusco',12,'negraso','2002-06-22'),(5,4,'Michifus',1,'Macho','gato','Balines',3,'Anaranjado','2002-07-22'),(6,5,'tomas',10,'Macho','gato','chuscofino',7,'gris','2002-08-22'),(7,5,'Felipe',6,'Macho','ave','loro',1,'verde','2002-09-22');
 /*!40000 ALTER TABLE `mascotas` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -120,7 +120,7 @@ DROP TABLE IF EXISTS `propietarios`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `propietarios` (
-  `idpropietario` int NOT NULL AUTO_INCREMENT,
+  `idpropietario` int NOT NULL,
   `nombre_propi` varchar(25) DEFAULT NULL,
   `apellido_propi` varchar(25) DEFAULT NULL,
   `direccion` varchar(25) DEFAULT NULL,
@@ -136,6 +136,7 @@ CREATE TABLE `propietarios` (
 
 LOCK TABLES `propietarios` WRITE;
 /*!40000 ALTER TABLE `propietarios` DISABLE KEYS */;
+INSERT INTO `propietarios` VALUES (1,'Alex','Winster','jr.la mauta 203','Alex@gmail.com',963852741),(2,'Martin','Perrez','jr.lima 2003','Martiper01@gmail.com',987654321),(3,'Lucho','Palacios','jr.la punta 207','Lucho@gmail.com',951632874),(4,'Milka','Rojas','jr.colectora 103','Milka@gmail.com',985263145),(5,'Ino','Acosta','jr.Alameda 301','Ino@gmail.com',987415623);
 /*!40000 ALTER TABLE `propietarios` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -148,58 +149,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2022-11-26 16:33:32
-
-/* CREATE DATABASE veterinaria;
-use veterinaria;
-CREATE TABLE mascotas(
-idmascota int not null auto_increment,
-id_Propietario int not null,
-nombre_mascota varchar(25),
-edad integer (2),
-sexo boolean,
-especie varchar (25),
-raza varchar (25),
-peso float (4),
-color_masc varchar (15),
-fecha_nacimiento date,
-vacunaciones date,
-primary key (idmascota),
-foreign key (id_Propietario) references propietarios(idpropietario));
-
-CREATE TABLE propietarios(
-idpropietario int not null auto_increment,
-nombre_propi varchar(25),
-apellido_propi varchar(25),
-direccion varchar(25),
-correo varchar(30),
-celular int(9),
-primary key (idpropietario));
-
-CREATE TABLE card(
-idcard int not null auto_increment,
-id_Mascota int not null,
-nombre_mascota varchar(25),
-edad integer (2),
-sexo boolean,
-especie varchar (25),
-raza varchar (25),
-peso float (4),
-color_masc varchar (15),
-fecha_nacimiento date,
-fecha_vacunacion date,
-tipo_vacuna varchar(25),
-observaciones varchar(40),
-primary key (idcard),
-foreign key (id_Mascota) references mascotas(idmascota)
-);
-
-CREATE TABLE medicoVeterinario(
-idveterinario int not null auto_increment,
-nombre_veterinario varchar(25),
-apellido_veterinario varchar(25),
-celular int(9),
-correo varchar(30),
-especialidad varchar(25),
-primary key (idveterinario));
- */;
+-- Dump completed on 2022-11-26 19:00:50
